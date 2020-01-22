29334c4d96a148a9ed57b22f9225239a05ab890d
Alejandro Sánchez Villarín <alesanvil3@alum.us.es>
2020-01-22 19:20:18 +0100

    FIx components and structure of html.

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-import Client from './components/Client';
-import Offers from './components/Offers';
-import Devices from './components/Devices';
-import Advertisement from './components/Advertisement';
-import Gift from './components/Gift';
 import * as serviceWorker from './serviceWorker';
-
-
-
-ReactDOM.render(<Client />, document.getElementById('client'));
-ReactDOM.render(<Offers />, document.getElementById('offers'));
-ReactDOM.render(<Devices />, document.getElementById('devices'));
-ReactDOM.render(<Advertisement />, document.getElementById('advert'));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
